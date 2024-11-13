b62ca427d94428c3892aaad30e798cbb3f2633cf
YE Chenyi <ye-ch564@school.lu>
2024-11-13 15:03:24 +0100

    test

--- Source/TopDownTest/TopDownTest.Build.cs
+++ Source/TopDownTest/TopDownTest.Build.cs
@@ -17,7 +17,6 @@ public class TopDownTest : ModuleRules
 															"Niagara",
 															"EnhancedInput",
 															"OnlineSubsystem",
-                                                            "OnlineSubsystemUtils",
                                                             "OnlineSubsystemNull",
 															"OnlineSubsystemSteam" });
     }
